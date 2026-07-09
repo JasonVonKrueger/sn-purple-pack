664d8440f46999db369910af040668f001b22b7a
System Administrator <admin@example.com>
2026-07-08 22:10:02 -0400

    updates

--- src/fluent/generated/keys.ts
+++ src/fluent/generated/keys.ts
@@ -89,9 +89,17 @@ declare global {
                         id: 'fc2c4031439e4b8d8f14b26861f33c1f'
                         deleted: true
                     }
+                    gitlab_branch: {
+                        table: 'sys_properties'
+                        id: 'bb2f1b5a82354d77a6a0892de81adaf5'
+                    }
                     gitlab_committer: {
                         table: 'sys_script_include'
                         id: '351753adbdda4a5f8dff8e9fb7383b47'
+                    }
+                    gitlab_directory: {
+                        table: 'sys_properties'
+                        id: 'b7e3fdcde46849b9b48a69981c63f421'
                     }
                     gitlab_project_id: {
                         table: 'sys_properties'
@@ -228,7 +236,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: '13da4c044706cb10b1197bb4416d434d'
-                        deleted: true
+                        deleted: false
                         key: {
                             name: 'var__m_sys_hub_flow_input_5fda4c044706cb10b1197bb4416d4303'
                             element: 'oauth_app_name'
@@ -273,7 +281,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: '1bda4c044706cb10b1197bb4416d433a'
-                        deleted: true
+                        deleted: false
                         key: {
                             model: '5fda4c044706cb10b1197bb4416d4303'
                             element: 'last_name'
@@ -316,7 +324,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: '2bda4c044706cb10b1197bb4416d437f'
-                        deleted: true
+                        deleted: false
                         key: {
                             name: 'var__m_sys_hub_flow_output_5fda4c044706cb10b1197bb4416d4303'
                             element: 'service_account_id'
@@ -534,7 +542,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: '50eacc044706cb10b1197bb4416d437e'
-                        deleted: true
+                        deleted: false
                         key: {
                             name: 'var__m_sys_hub_flow_input_54eacc044706cb10b1197bb4416d436f'
                             element: 'current'
@@ -544,7 +552,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_output'
                         id: '53da4c044706cb10b1197bb4416d436c'
-                        deleted: true
+                        deleted: false
                         key: {
                             model: '5fda4c044706cb10b1197bb4416d4303'
                             element: 'success'
@@ -569,7 +577,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: '5bda4c044706cb10b1197bb4416d4341'
-                        deleted: true
+                        deleted: false
                         key: {
                             name: 'var__m_sys_hub_flow_input_5fda4c044706cb10b1197bb4416d4303'
                             element: 'last_name'
@@ -579,7 +587,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: '5bda4c044706cb10b1197bb4416d4358'
-                        deleted: true
+                        deleted: false
                         key: {
                             name: 'var__m_sys_hub_flow_input_5fda4c044706cb10b1197bb4416d4303'
                             element: 'email'
@@ -611,7 +619,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: '5fda4c044706cb10b1197bb4416d432a'
-                        deleted: true
+                        deleted: false
                         key: {
                             name: 'var__m_sys_hub_flow_input_5fda4c044706cb10b1197bb4416d4303'
                             element: 'username'
@@ -621,7 +629,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: '621a5a7e47398310b1197bb4416d4355'
-                        deleted: true
+                        deleted: false
                         key: {
                             name: 'var__m_sys_hub_flow_input_c5a20f8a14654ff5ad64263b7dc05446'
                             element: 'table_name'
@@ -665,7 +673,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_output'
                         id: '6bda4c044706cb10b1197bb4416d4378'
-                        deleted: true
+                        deleted: false
                         key: {
                             model: '5fda4c044706cb10b1197bb4416d4303'
                             element: 'service_account_id'
@@ -851,7 +859,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: '93da4c044706cb10b1197bb4416d4336'
-                        deleted: true
+                        deleted: false
                         key: {
                             name: 'var__m_sys_hub_flow_input_5fda4c044706cb10b1197bb4416d4303'
                             element: 'first_name'
@@ -861,7 +869,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: '93da4c044706cb10b1197bb4416d4364'
-                        deleted: true
+                        deleted: false
                         key: {
                             name: 'var__m_sys_hub_flow_input_5fda4c044706cb10b1197bb4416d4303'
                             element: 'redirect_url'
@@ -871,7 +879,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: '94eacc044706cb10b1197bb4416d4389'
-                        deleted: true
+                        deleted: false
                         key: {
                             name: 'var__m_sys_hub_flow_input_54eacc044706cb10b1197bb4416d436f'
                             element: 'table_name'
@@ -907,7 +915,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: '97da4c044706cb10b1197bb4416d4351'
-                        deleted: true
+                        deleted: false
                         key: {
                             model: '5fda4c044706cb10b1197bb4416d4303'
                             element: 'email'
@@ -916,7 +924,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: '98eacc044706cb10b1197bb4416d4371'
-                        deleted: true
+                        deleted: false
                         key: {
                             model: '54eacc044706cb10b1197bb4416d436f'
                             element: 'current'
@@ -991,7 +999,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_output'
                         id: 'afda4c044706cb10b1197bb4416d4383'
-                        deleted: true
+                        deleted: false
                         key: {
                             model: '5fda4c044706cb10b1197bb4416d4303'
                             element: 'oauth_app_id'
@@ -1000,7 +1008,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: 'afda4c044706cb10b1197bb4416d438a'
-                        deleted: true
+                        deleted: false
                         key: {
                             name: 'var__m_sys_hub_flow_output_5fda4c044706cb10b1197bb4416d4303'
                             element: 'oauth_app_id'
@@ -1159,7 +1167,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: 'd21a5a7e47398310b1197bb4416d4309'
-                        deleted: true
+                        deleted: false
                         key: {
                             model: 'c5a20f8a14654ff5ad64263b7dc05446'
                             element: 'current'
@@ -1176,7 +1184,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: 'd3da4c044706cb10b1197bb4416d432f'
-                        deleted: true
+                        deleted: false
                         key: {
                             model: '5fda4c044706cb10b1197bb4416d4303'
                             element: 'first_name'
@@ -1185,7 +1193,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: 'd3da4c044706cb10b1197bb4416d4374'
-                        deleted: true
+                        deleted: false
                         key: {
                             name: 'var__m_sys_hub_flow_output_5fda4c044706cb10b1197bb4416d4303'
                             element: 'success'
@@ -1195,7 +1203,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: 'd4eacc044706cb10b1197bb4416d4382'
-                        deleted: true
+                        deleted: false
                         key: {
                             model: '54eacc044706cb10b1197bb4416d436f'
                             element: 'table_name'
@@ -1212,7 +1220,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: 'da1a5a7e47398310b1197bb4416d4324'
-                        deleted: true
+                        deleted: false
                         key: {
                             name: 'var__m_sys_hub_flow_input_c5a20f8a14654ff5ad64263b7dc05446'
                             element: 'current'
@@ -1222,7 +1230,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: 'dbda4c044706cb10b1197bb4416d4313'
-                        deleted: true
+                        deleted: false
                         key: {
                             model: '5fda4c044706cb10b1197bb4416d4303'
                             element: 'username'
@@ -1231,7 +1239,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: 'de1a5a7e47398310b1197bb4416d4328'
-                        deleted: true
+                        deleted: false
                         key: {
                             model: 'c5a20f8a14654ff5ad64263b7dc05446'
                             element: 'table_name'
@@ -1249,7 +1257,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: 'dfda4c044706cb10b1197bb4416d4345'
-                        deleted: true
+                        deleted: false
                         key: {
                             model: '5fda4c044706cb10b1197bb4416d4303'
                             element: 'oauth_app_name'
@@ -1258,7 +1266,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: 'dfda4c044706cb10b1197bb4416d435c'
-                        deleted: true
+                        deleted: false
                         key: {
                             model: '5fda4c044706cb10b1197bb4416d4303'
                             element: 'redirect_url'
--- src/fluent/system-properties/gitlab.now.ts
+++ src/fluent/system-properties/gitlab.now.ts
@@ -6,12 +6,14 @@ Record({
     table: 'sys_properties',
     data: {
         name: 'x_1892699_purple_pack.gitlab.url',
-        value: '',
+        value: 'https://gitlab.com/servicenow8703321/update-sets.git',
         description: 'GitLab base URL (e.g. https://gitlab.example.com)',
         type: 'string',
         suffix: 'gitlab.url',
         read_roles: ['admin'],
         write_roles: ['admin'],
+        ignore_cache: false,
+        is_private: false,
     },
 })
 
@@ -20,12 +22,15 @@ Record({
     table: 'sys_properties',
     data: {
         name: 'x_1892699_purple_pack.gitlab.token',
-        value: '',
+        value: `{{dflt-gpaes}}SI1M3/30Ifa5JZgt0NOm84yQ/CU4eeERMnoNGXVTPPCE9JVVeWZ7BvIUFYaJ98pBUGmDXB7mNzfX\r
+VxtjHYO59w==`,
         description: 'Personal / project access token for GitLab API authentication',
         type: 'password2',
         suffix: 'gitlab.token',
         read_roles: ['admin'],
         write_roles: ['admin'],
+        ignore_cache: false,
+        is_private: false,
     },
 })
 
@@ -34,11 +39,45 @@ Record({
     table: 'sys_properties',
     data: {
         name: 'x_1892699_purple_pack.gitlab.project_id',
-        value: '',
+        value: '82507261',
         description: 'Numeric project ID or URL-encoded namespace/project path in GitLab',
         type: 'string',
         suffix: 'gitlab.project_id',
         read_roles: ['admin'],
         write_roles: ['admin'],
+        ignore_cache: false,
+        is_private: false,
+    },
+})
+
+Record({
+    $id: Now.ID['gitlab_branch'],
+    table: 'sys_properties',
+    data: {
+        name: 'x_1892699_purple_pack.gitlab.branch',
+        value: 'main',
+        description: 'GitLab branch for update sets',
+        type: 'string',
+        suffix: 'gitlab.branch',
+        read_roles: ['admin'],
+        write_roles: ['admin'],
+        ignore_cache: false,
+        is_private: false,
+    },
+})
+
+Record({
+    $id: Now.ID['gitlab_directory'],
+    table: 'sys_properties',
+    data: {
+        name: 'x_1892699_purple_pack.gitlab.directory',
+        value: 'update-sets',
+        description: 'GitLab directory for update sets',
+        type: 'string',
+        suffix: 'gitlab.directory',
+        read_roles: ['admin'],
+        write_roles: ['admin'],
+        ignore_cache: false,
+        is_private: false,
     },
 })
